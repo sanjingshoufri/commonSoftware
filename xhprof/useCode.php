374650f87f7fd7eaea3b5186f5ace3392a2d39b9
TANNER <2303386665@qq.com>
2020-05-25 18:22:26 +0800

    xhprof

--- xhprof/useCode.php
+++ xhprof/useCode.php
@@ -10,4 +10,5 @@ $xhprof_runs = new XHProfRuns_Default();
 // 导出性能分析数据，默认xhprof.output_dir指定的目录
 $run_id = $xhprof_runs->save_run($xhprof_data, 'xhprof');
 
-// 
+// 关闭xhprof.
+$xhprof_data = xhprof_disable();
